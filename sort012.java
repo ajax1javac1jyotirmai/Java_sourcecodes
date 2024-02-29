25254f1388e1beca36e383fb65a40007608279e9
ajax1javac1jyotirmai <161692800+ajax1javac1jyotirmai@users.noreply.github.com>
2024-02-29 16:18:25 +0530

    Create sort012.java

---
+++ sort012.java
@@ -0,0 +1,34 @@
+
+    static void sort012(int a[], int arr_size)
+    {
+        int lo = 0;
+        int hi = arr_size - 1;
+        int mid = 0, temp = 0;
+        // Iterate till all the elements
+        // are sorted
+        while (mid <= hi) {
+            switch (a[mid]) {
+                // If the element is 0
+            case 0: {
+                temp = a[lo];
+                a[lo] = a[mid];
+                a[mid] = temp;
+                lo++;
+                mid++;
+                break;
+            }
+                // If the element is 1
+            case 1:
+                mid++;
+                break;
+                // If the element is 2
+            case 2: {
+                temp = a[mid];
+                a[mid] = a[hi];
+                a[hi] = temp;
+                hi--;
+                break;
+            }
+            }
+        }
+    }
