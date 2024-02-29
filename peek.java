325813ad8c329f2bf3c3cee65ad03e0bafd91d34
ajax1javac1jyotirmai <161692800+ajax1javac1jyotirmai@users.noreply.github.com>
2024-02-29 14:26:41 +0530

    Create peek.java
    len===1 : no peak
    ends test 
    between test

---
+++ peek.java
@@ -0,0 +1,18 @@
+  public static void main(String[] args) {
+        int ar[]={1,3,20,4,1,0};
+        //peek element
+        int len1=ar.length;
+        if(len1==1):return 0;
+        if(ar[0]>ar[1]): return 0;
+        if(ar[n-1]>ar[n-2]):return n-1;
+        for(int i=0;i<n-1;i++){
+            if(ar[i]>ar[i-1]){
+                return i;
+            }
+        }
+        return 0;
+        
+
+    
+        
+    }
